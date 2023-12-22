5baac75b32993651c5c70207edf1109bd9a5d9ab
9thdat <nthdat09@gmail.com>
2023-12-22 22:32:38 +0700

    Update readme.md

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,3 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://localhost:5432/api",
 });
-
-export const locationApi = axios.create({
-    baseURL: "https://online-gateway.ghn.vn/shiip/public-api/master-data",
-});
